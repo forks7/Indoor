a6f560462e2cdacd4683761e57f7e49b4ab7aba3
Ryan Famorca <ryan.famorca@gmail.com>
2015-07-26 12:45:53 +1000

    first test
    testing git hub

---
+++ Indoor/DivisionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DivisionsViewController.swift
+//  Indoor
+//
+//  Created by Ryan Famorca on 20/07/2015.
+//  Copyright (c) 2015 com.ryanfamorca. All rights reserved.
+//
+
+import UIKit
+
+class DivisionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "DivisionBackground")!)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Indoor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Indoor
+//
+//  Created by Ryan Famorca on 19/06/2015.
+//  Copyright (c) 2015 com.ryanfamorca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Page1.jpg")!);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
